ce205dd0722a3e4840e04bbee068fbf1fa9653cd
Mika Finkman <mf654@cornell.edu>
2025-03-05 13:17:25 -0500

    partial server side encryption complete

--- types/server_message.go
+++ types/server_message.go
@@ -5,8 +5,13 @@ import (
 )
 
 type Server_Message struct {
-	Server     string    `json:"server"`
+	Client     string    `json:"client"`
 	Uid        string    `json:"uid"`
 	S_Response Response  `json:"s_response"`
 	Tod        time.Time `json:"tod"`
+}
+
+type Encrypted_Response struct {
+	Server       string `json:"server"`
+	Enc_Signed_M []byte `json:"signed_message"`
 }
