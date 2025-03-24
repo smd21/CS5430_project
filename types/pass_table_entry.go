e7537d5b3eb74bdf50da21bfb1a411548511f206
smd21 <serenaduncan24@gmail.com>
2025-03-24 14:57:00 -0400

    unused import

--- types/pass_table_entry.go
+++ types/pass_table_entry.go
@@ -1,15 +1,11 @@
 package types
 
-import (
-	"time"
-)
-
 type Hashed_Password struct {
 	Password string `json:"pass"`
-	Salt []byte `json:"salt"`
+	Salt     []byte `json:"salt"`
 }
 
 type Password_Table_Entry struct {
-	Uid string `json:"uid"`
-	Hashpass        []byte    `json:"hashed_password"`
+	Uid      string `json:"uid"`
+	Hashpass []byte `json:"hashed_password"`
 }
